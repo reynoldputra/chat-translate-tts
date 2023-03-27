a12c1a8a53a2af1473b5802baa4cf52b8108975e
reynoldputra <reynoldputra1@gmail.com>
2023-03-27 19:31:54 +0700

    fix: ui error

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,67 +1,77 @@
 <template>
-  <div class="min-h-screen bg-blue-200">
+  <div class="h-screen bg-blue-200">
     <div class="px-10 py-4">
       <img src="../assets/img/logo.png" />
     </div>
-    <div class="flex justify-between h-[750px] px-10">
-      <div class="h-full w-1/4 pb-10 flex flex-col text-[#2B4FA0] justify-end items-center text-center gap-8">
-        <img src="../assets/img/img-1.png" alt="">
-        <div>
-          <p class="pb-2 text-[#2B4FA0] font-bold">First person language</p>
-          <select v-model="input.lang.first" class="py-2 px-16 rounded-[28px]" name="" id="">
-            <option v-for="(lang, idx) in languageList" :value="idx">{{ lang.name }}</option>
-          </select>
+    <div class="flex justify-between px-10">
+      <div class="h-full p-6 flex flex-col text-[#2B4FA0] justify-end items-center text-center gap-4">
+        <img class="w-60" src="../assets/img/img-1.png" alt="">
+        <div class="flex gap-4">
+          <div>
+            <p class="pb-2 text-[#2B4FA0] font-bold">First person language</p>
+            <select v-model="input.lang.first" class="py-2 w-18 px-16 rounded-[28px]" name="" id="">
+              <option v-for="(lang, idx) in languageList" :value="idx">{{ lang.name }}</option>
+            </select>
+          </div>
+          <div>
+            <p class="pb-2 text-[#2B4FA0] font-bold">Second person language</p>
+            <select v-model="input.lang.second" class="py-2 px-16 w-18 rounded-[28px]" name="" id="">
+              <option v-for="(lang, idx) in languageList" :value="idx">{{ lang.name }}</option>
+            </select>
+          </div>
         </div>
         <div>
-          <p class="pb-2 text-[#2B4FA0] font-bold">Second person language</p>
-          <select v-model="input.lang.second" class="py-2 px-16 rounded-[28px]" name="" id="">
-            <option v-for="(lang, idx) in languageList" :value="idx">{{ lang.name }}</option>
-          </select>
-        </div>
-        <div>
-          <p class="pb-2 text-[#2B4FA0] font-bold">Choose voice</p>
-          <select v-model="input.voice" class="py-2 px-6 rounded-[28px]" name="" id="">
-            <option v-for="(lang, idx) in languageList" :value="lang.voice">{{ lang.voice }}</option>
-          </select>
+          <div>
+            <p class="pb-2 text-[#2B4FA0] font-bold">Choose first person voice</p>
+            <select v-model="input.voice.first" class="py-2 px-6 rounded-[28px]" name="" id="">
+              <option v-for="(lang, idx) in languageList" :value="lang.voice">{{ lang.voice }}</option>
+            </select>
+          </div>
+          <div>
+            <p class="pb-2 text-[#2B4FA0] font-bold pt-4">Choose second person voice</p>
+            <select v-model="input.voice.second" class="py-2 px-6 rounded-[28px]" name="" id="">
+              <option v-for="(lang, idx) in languageList" :value="lang.voice">{{ lang.voice }}</option>
+            </select>
+          </div>
         </div>
           <p class="text-[#2B4FA0] font-bold">Person is typing ... </p>
           <p class="bg-white py-4 px-8 rounded-full text-lg text-[#2B4FA0] font-bold">{{person}}</p>
       </div>
-      <div class="w-[1300px] rounded-[60px] p-10 bg-blue-100 flex flex-col justify-between">
+      <div class="w-[850px] rounded-[32px] p-10 bg-blue-100 flex flex-col justify-between">
         <div>
           <div v-for="chat in chats">
             <div v-if="chat.person == 1" class="flex mb-8">
-              <div class="bg-white rounded-[40px] py-8 px-12 rounded-bl-sm w-fit min-w-[600px]">
-                <p class="text-slate-600">{{chat.textInput}}</p>
-                <p class="text-xl font-bold">{{chat.textOutput}}</p>
+              <div class="bg-white rounded-[24px] py-2 px-4 rounded-bl-sm w-fit min-w-[400px]">
+                <p class="text-sm text-slate-600">{{chat.textInput}}</p>
+                <p class="text-md font-bold">{{chat.textOutput}}</p>
               </div>
-              <div class="w-16 h-16 my-auto ml-12 rounded-full bg-white"  @click="playVoice(chat.textOutput)">
-                <img src="../assets/img/speaker.png" class="m-auto my-auto py-4" alt="" />
+              <div class="w-12 h-12 my-auto ml-12 rounded-full bg-white"  @click="playVoice(chat)">
+                <img src="../assets/img/speaker.png" class=" w-6 m-auto my-auto py-3 " alt="" />
               </div>
             </div>
-            <div v-if="chat.person ==2" class="flex justify-end mb-8 text-white">
-              <div class="w-16 h-16 my-auto mr-12 rounded-full bg-[#2B4FA0]"  @click="playVoice(chat.textOutput)">
-                <img src="../assets/img/speaker-white.png" class="m-auto my-auto py-4" alt="" />
+            <div v-if="chat.person == 2" class="flex justify-end mb-8 text-white">
+              <div class="w-12 h-12 my-auto mr-12 rounded-full bg-[#2B4FA0]"  @click="playVoice(chat)">
+                <img src="../assets/img/speaker-white.png" class=" w-6 m-auto my-auto py-3" alt="" />
               </div>
-              <div class="bg-[#2B4FA0] rounded-[40px] py-8 px-12 rounded-br-sm w-fit min-w-[600px]">
-                <p class="text-blue-200">{{chat.textInput}}</p>
-                <p class="text-xl font-bold">{{chat.textOutput}}</p>
+              <div class="bg-[#2B4FA0] rounded-[24px] py-2 px-4 rounded-br-sm w-fit min-w-[400px]">
+                <p class="text-sm text-blue-200">{{chat.textInput}}</p>
+                <p class="text-md font-bold">{{chat.textOutput}}</p>
               </div>
             </div>
           </div>
         </div>
-        <div class="bg-white p-4 pl-8 flex justify-between rounded-[60px]">
+        <div class="bg-white p-2 pl-8 flex justify-between rounded-[60px]">
           <div class="flex">
-            <div @click="changePerson(1)" class="font-bold rounded-full w-14 h-14 my-auto text-center bg-blue-300 p-4">
+            <div @click="changePerson(1)" class="font-bold rounded-full w-8 h-8 my-auto text-center bg-blue-300 pt-1">
               1
             </div>
-            <div @click="changePerson(2)" class="bg-[#2B4FA0] text-white font-bold rounded-full w-14 h-14 my-auto ml-4 text-center p-4">
+            <div @click="changePerson(2)" class="bg-[#2B4FA0] text-white font-bold rounded-full ml-4 w-8 h-8 my-auto text-center pt-1">
               2
             </div>
-            <input v-model="input.text" class=" w-[500px] px-4 text-xl ml-6" type="text" placeholder="Type your text here ..." />
+            <input v-model="input.text" class=" w-[500px] px-4 text-sm ml-6" type="text" placeholder="Type your text here ..." />
           </div>
-          <div @click="translate" class="rounded-full bg-blue-300 p-4">
-            <img src="../assets/img/sent.png" alt="" />
+          <div @click="translate" class="rounded-full bg-blue-300 p-3">
+            <img src="../assets/img/sent.png" alt="" class="w-6" />
           </div>
         </div>
       </div>
@@ -84,7 +94,11 @@ export default {
           second : 3
         },
         text : null,
-        voice : languageList[0].voice
+        voice : {
+          first :  languageList[0].voice,
+          first :  languageList[0].voice
+        }
+
       },
       output : {
         text : null
@@ -146,20 +160,22 @@ export default {
               textOutput : this.output.text
             } 
           )
-          console.log(this.chats)
+          this.input.text = ""
         })
         .catch( (error) => {
           console.error(error)
         })
 
     },
-    async playVoice(text) {
+    async playVoice(chat) {
+      console.log(chat)
       let choosedVoice = null;
       const voiceList = this.voiceList
-      console.log(this.input.voice)
+      let inputVoice = null
+      if(chat.person == 1) inputVoice = this.input.voice.first
+      else inputVoice = this.input.voice.second
       voiceList.forEach(voice => {
-        console.log(voice.name)
-        if(voice.name == this.input.voice) choosedVoice = voice
+        if(voice.name == inputVoice ) choosedVoice = voice
       })
       console.log(choosedVoice)
       await new Promise((resolve, reject) => {
@@ -174,8 +190,9 @@ export default {
           this.isPlay = false
           reject()
         })
-        const voice = new SpeechSynthesisUtterance(text)
+        const voice = new SpeechSynthesisUtterance(chat.textOutput)
         voice.voice = choosedVoice
+        console.log(voice)
         tts.speak(voice)
       })
     },
